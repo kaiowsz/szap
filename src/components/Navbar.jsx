6a340480f0602f357cd32a9d5aed8cd6bbd8ab2a
kaiowsz <kaionpsa@gmail.com>
2023-01-05 18:28:33 -0300

    package.json changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import tate from "../img/tate.jpg"
 import { signOut } from "firebase/auth"
 import { auth } from "../firebase"
 import { AuthContext } from "../context/AuthContext"
